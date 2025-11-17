398cae7b20f71abf49a83f7574db43fdc61f13ac
febianprima <arprim13@gmail.com>
2025-11-18 02:10:20 +0800

    task: create a simple home screen design

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,17 +1,36 @@
-import React, { useContext } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
-import { AuthenticationContext } from '../contexts/AuthenticationContext';
+import { SignatureIcon } from 'lucide-react-native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 const HomeScreen = () => {
-  const { logout } = useContext(AuthenticationContext);
   return (
-    <View>
-      <Text>Home</Text>
-      <TouchableOpacity onPress={logout}>
-        <Text>Logout</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <SignatureIcon size={100} color="teal" />
+      <Text style={styles.title}>It's good to see you again, A!</Text>
+      <Text style={styles.subtitle}>I've sent a verification code to abc@mail.com</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 16,
+    paddingHorizontal: 24,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center',
+  },
+  subtitle: {
+    fontSize: 20,
+    color: 'gray',
+    textAlign: 'center',
+  },
+});
 
 export default HomeScreen;
